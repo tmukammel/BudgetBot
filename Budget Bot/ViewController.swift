4da594b27fe683e10fe5f533cd84123df29bc58b
Twaha Mukammel <t.mukammel@aol.com>
2017-01-25 20:23:48 +0600

    - added viewDidApper in ViewController

--- Budget Bot/ViewController.swift
+++ Budget Bot/ViewController.swift
@@ -23,5 +23,9 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
 }
 
